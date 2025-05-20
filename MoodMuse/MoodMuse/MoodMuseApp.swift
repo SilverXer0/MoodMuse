dc3f35af1c08b3c0fa5017a78981cd48840d6ed8
Sharan Krishna <87695707+SilverXer0@users.noreply.github.com>
2025-05-20 01:05:05 -0700

    feat: created app and added gitignore

---
+++ MoodMuse/MoodMuse/MoodMuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMuseApp.swift
+//  MoodMuse
+//
+//  Created by Sharan Krishna on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodMuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodMuse/MoodMuseTests/MoodMuseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMuseTests.swift
+//  MoodMuseTests
+//
+//  Created by Sharan Krishna on 5/20/25.
+//
+
+import Testing
+@testable import MoodMuse
+
+struct MoodMuseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
